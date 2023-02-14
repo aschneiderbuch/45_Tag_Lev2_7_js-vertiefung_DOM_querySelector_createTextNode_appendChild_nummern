ff5dcb687f4854d4078d5c8fec684e6fd8e4b811
aschneiderbuch <aschneiderbuch2@gmail.com>
2023-02-14 23:49:24 +0100

    click

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,2 +1,44 @@
 console.log("test");
 
+/* 
+querySelector()
+addEventListener()
+let counter = 0;
+createElement()
+textContent()
+appendChild()
+classList
+
+*/
+// click 
+
+const btn = document.querySelector("button");
+console.log(btn);
+btn.addEventListener("click", (e) => {
+    ausfüren();
+});
+
+    // Element mit aufsteigender Nummer
+    nummern = [];
+for (let i = 1 ; i < 1001; i++) {
+    console.log(i);
+    nummern.push(i);
+    console.log(nummern);  // [1], ... [1001]
+}
+
+// function
+function ausfüren(e) {
+
+    outputNummer = nummern.shift(); // holt sich erste [1]
+    console.log(outputNummer);   
+    console.log(nummern);
+
+// ins HTML
+
+
+
+};
+
+
+
+
