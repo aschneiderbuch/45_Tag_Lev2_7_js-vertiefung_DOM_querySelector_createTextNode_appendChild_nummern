9bdf61f78ea8571b4d67e27e4fef4d3660b12509
aschneiderbuch <aschneiderbuch2@gmail.com>
2023-02-15 00:12:43 +0100

    if

--- assets/js/main.js
+++ assets/js/main.js
@@ -10,32 +10,78 @@ appendChild()
 classList
 
 */
-// click 
 
 const btn = document.querySelector("button");
 console.log(btn);
+
+const outputField = document.querySelector("div");
+
+
+
+/************************************************************************************************
+ *      
+ *                       Ende  Variablen
+ * 
+ ***********************************************************************************************/
+
+
+
+
+// click 
 btn.addEventListener("click", (e) => {
-    ausfüren();
+    ausfüren(e);
 });
 
-    // Element mit aufsteigender Nummer
-    nummern = [];
-for (let i = 1 ; i < 1001; i++) {
+// Element mit aufsteigender Nummer
+nummern = [];
+for (let i = 0; i < 1001; i++) {
     console.log(i);
     nummern.push(i);
-    console.log(nummern);  // [1], ... [1001]
+    console.log(nummern);  // [0], ... [1001]
 }
 
 // function
 function ausfüren(e) {
 
+
     outputNummer = nummern.shift(); // holt sich erste [1]
-    console.log(outputNummer);   
+    console.log(outputNummer);
     console.log(nummern);
 
-// ins HTML
+
+    // HTML Element erzeugen
+    const pTag = document.createElement("p");
+    console.log(pTag);
+
+    // Element Text zuweisen
+    pTag.textContent = outputNummer;
+    console.log(pTag);
 
 
+    // ins HTML
+    //  outputField.appendChild(pTag);
+
+    // davor css Style verpassen
+    // 0 + alle 10er rund = .weiß     der rest rechteck = .rechteck
+    if (pTag == 0) {
+        console.log("0 ")
+        pTag.classList.add("rechteck");
+        pTag.classList.add("weiss");
+    }
+    else if (pTag % 10 == 0) {
+        console.log(" 10er")
+        pTag.classList.add("rechteck");
+        pTag.classList.add("weiss");
+    }
+    else {
+        console.log("1-9er")
+        pTag.classList.add("rechteck");
+
+    }
+
+
+    // ins HTML
+    outputField.appendChild(pTag);
 
 };
 
